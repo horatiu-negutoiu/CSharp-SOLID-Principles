3e68bb8862c60bbd6783ce5cb8668471f9ab5334
Horatiu Negutoiu <admin@horatiu.ca>
2018-09-21 02:23:29 -0400

    stage 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,24 +7,28 @@
     {
         static void Main(string[] args)
         {
-            List<Transaction> transactions = new List<Transaction>
-            {
-                new Transaction(0, 10.40),
-                new Transaction(1, -5.00),
-            };
+            Dictionary<int, ITransaction> transactions = new Dictionary<int, ITransaction>();
+            transactions.Add(0, new FoodTransaction(0, 10.40));
+            transactions.Add(1, new TransportationTransaction(1, -5.00));
 
-            foreach (Transaction transaction in transactions)
+            foreach (KeyValuePair<int, ITransaction> transactionEntry in transactions)
             {
-                transaction.OutputTransaction();
+                transactionEntry.Value.OutputTransaction();
             }
             Console.ReadKey();
         }
     }
 
-    class Transaction
+    interface ITransaction
+    {
+        void OutputTransaction();
+    }
+
+    class Transaction : ITransaction
     {
         public int id;
         public double amount;
+        public string type;
         public Writer writer;
 
         public Transaction(int id, double amount)
@@ -40,6 +44,28 @@
         }
     }
 
+    class FoodTransaction : Transaction, ITransaction
+    {
+        public FoodTransaction(int id, double amount) : base(id, amount)
+        {
+            this.id = id;
+            this.amount = amount;
+            this.type = "food";
+            this.writer = new Writer();
+        }
+    }
+
+    class TransportationTransaction : Transaction, ITransaction
+    {
+        public TransportationTransaction(int id, double amount) : base(id, amount)
+        {
+            this.id = id;
+            this.amount = amount;
+            this.type = "transportation";
+            this.writer = new Writer();
+        }
+    }
+
     class Writer
     {
         public void WriteTransaction(Transaction transaction)
@@ -49,7 +75,14 @@
 
         public void WriteTransactionToConsole(Transaction transaction)
         {
-            Console.WriteLine($"Transaction [{transaction.id.ToString()}] - ${transaction.amount.ToString()}");
+            if (transaction.type == "food")
+            {
+                Console.WriteLine($"Food: ${transaction.amount}");
+            }
+            else if (transaction.type == "transportation")
+            {
+                Console.WriteLine($"Transportation: ${transaction.amount}");
+            }
         }
     }
 }
